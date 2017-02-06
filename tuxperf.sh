6c35e96b1e6e1fe04fc231c1d2f31151d1fc4bc5
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-02-07 00:35:51 +0100

    Added some links as comments

--- tuxperf.sh
+++ tuxperf.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# http://techblog.netflix.com/2015/11/linux-performance-analysis-in-60s.html
+# http://techblog.netflix.com/2015/04/introducing-vector-netflixs-on-host.html
+
+
 printf "x========[ System Uptime ]========================================================================================\n"
 uptime -p
 
