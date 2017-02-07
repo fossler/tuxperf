2bf620c971baefbdf43dee733cf4f8a8d247a5e4
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-02-07 16:34:29 +0100

    ascii colors testing

--- tuxperf.sh
+++ tuxperf.sh
@@ -4,11 +4,11 @@
 # http://techblog.netflix.com/2015/04/introducing-vector-netflixs-on-host.html
 
 
-echo -e "\e[31;43m****** SYSTEM UPTIME AND LOAD ******\e[0m"
+echo -e "\e[31;30m****** SYSTEM UPTIME AND LOAD ******\e[0m"
 uptime -p
 echo ""
 
-echo -e "\e[31;43m****** dmesg - last 10 lines ]******\e[0m"
+echo -e "\e[31;43m******[ dmesg - last 10 lines ]******\e[0m"
 dmesg | tail
 
 echo -e "\e[31;43m ...:::[ TOP 10 Processes by Memory ]:::...\e[0m"
