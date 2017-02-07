cf8852ba5668396b294101b62d5357eb82e29d62
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-02-07 17:25:23 +0100

    ascii testing

--- tuxperf.sh
+++ tuxperf.sh
@@ -4,7 +4,7 @@
 # http://techblog.netflix.com/2015/04/introducing-vector-netflixs-on-host.html
 
 
-echo -e "\e[31;30m****** SYSTEM UPTIME AND LOAD ******\e[0m"
+echo -e "\e[30;43m****** SYSTEM UPTIME AND LOAD ******\e[0m"
 uptime -p
 echo ""
 
@@ -14,8 +14,8 @@ dmesg | tail
 echo -e "\e[31;43m ...:::[ TOP 10 Processes by Memory ]:::...\e[0m"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
 
-echo -e "\e[31;43m ######[ TOP 10 Processes by Memory ]######\e[0m"
+echo -e "\e[31;43m ᐊ ᐊ ᐊ ᐊ ᐊ ᐊ [ TOP 10 Processes by Memory ] ᐅ ᐅ ᐅ ᐅ ᐅ ᐅ \e[0m"
 echo ""
 echo ""
 echo ""
-echo -e "\e[31;30m‡‡‡‡‡‡[ SYSTEM UPTIME AND LOAD ]‡‡‡‡‡‡\e[0m"
+echo -e "\e[31;43m‡‡‡‡‡‡[ SYSTEM UPTIME AND LOAD ]‡‡‡‡‡‡\e[0m"
