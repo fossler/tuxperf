04544e8b49a46c4ab5db7c4cb8417aa65fb800c3
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-02-07 16:58:37 +0100

    asci design testing

--- tuxperf.sh
+++ tuxperf.sh
@@ -8,10 +8,14 @@ echo -e "\e[31;30m****** SYSTEM UPTIME AND LOAD ******\e[0m"
 uptime -p
 echo ""
 
-echo -e "\e[31;43m******[ dmesg - last 10 lines ]******\e[0m"
+echo -e "\e[31;43m••••••[ dmesg - last 10 lines ]••••••\e[0m"
 dmesg | tail
 
 echo -e "\e[31;43m ...:::[ TOP 10 Processes by Memory ]:::...\e[0m"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
 
 echo -e "\e[31;43m ######[ TOP 10 Processes by Memory ]######\e[0m"
+echo ""
+echo ""
+echo ""
+echo -e "\e[31;30m‡‡‡‡‡‡[ SYSTEM UPTIME AND LOAD ]‡‡‡‡‡‡\e[0m"
