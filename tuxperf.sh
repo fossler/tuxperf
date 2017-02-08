d72f41293772f939a089298c068e463546b29303
Mirzet Kadic <mirzet.kadic@gmail.com>
2017-02-08 13:15:42 +0100

    ascii design defined

--- tuxperf.sh
+++ tuxperf.sh
@@ -2,20 +2,16 @@
 
 # http://techblog.netflix.com/2015/11/linux-performance-analysis-in-60s.html
 # http://techblog.netflix.com/2015/04/introducing-vector-netflixs-on-host.html
+clear
 
-
-echo -e "\e[30;43m****** SYSTEM UPTIME AND LOAD ******\e[0m"
+echo -e "\e[30;43m\e[1m *************[ SYSTEM UPTIME AND LOAD ]************* \e[0m"
 uptime -p
 echo ""
 
-echo -e "\e[31;43m••••••[ dmesg - last 10 lines ]••••••\e[0m"
+echo -e "\e[30;43m\e[1m *************[ dmesg - last 10 lines ]************* \e[0m"
 dmesg | tail
+echo ""
 
-echo -e "\e[31;43m ...:::[ TOP 10 Processes by Memory ]:::...\e[0m"
+echo -e "\e[30;43m\e[1m *************[ TOP 10 Processes by Memory ]************* \e[0m"
 ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head
-
-echo -e "\e[31;43m ᐊ ᐊ ᐊ ᐊ ᐊ ᐊ [ TOP 10 Processes by Memory ] ᐅ ᐅ ᐅ ᐅ ᐅ ᐅ \e[0m"
 echo ""
-echo ""
-echo ""
-echo -e "\e[31;43m‡‡‡‡‡‡[ SYSTEM UPTIME AND LOAD ]‡‡‡‡‡‡\e[0m"
